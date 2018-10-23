46c4dd4e86a58cc44dd86540149fda361d9c2d31
JAMESSS\JamessS <congdv555@gmail.com>
2018-10-23 21:00:53 +0700

    add delete function

--- app/src/main/java/vn/edu/fpt/kidapp/ViewHistoryActivity.java
+++ app/src/main/java/vn/edu/fpt/kidapp/ViewHistoryActivity.java
@@ -1,6 +1,8 @@
 package vn.edu.fpt.kidapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,6 +24,7 @@ import vn.edu.fpt.kidapp.database.DBManager;
 
 public class ViewHistoryActivity extends AppCompatActivity {
     private GridView grvHistory;
+    private DBManager db;
     private List<CapturePicture> listPicture;
     private PictureHistoryAdapter adapter;
 
@@ -31,7 +34,7 @@ public class ViewHistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_history);
         grvHistory = findViewById(R.id.grvHistory);
 
-        DBManager db = new DBManager(this);
+         db = new DBManager(this);
         listPicture = db.getAllPicture();
         adapter = new PictureHistoryAdapter(this, R.layout.picture_history_item_layout, listPicture);
         grvHistory.setAdapter(adapter);
@@ -55,7 +58,7 @@ public class ViewHistoryActivity extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
-                showPopup(view);
+                showPopup(view, position);
 
 
 
@@ -67,10 +70,60 @@ public class ViewHistoryActivity extends AppCompatActivity {
     }
 
 
-    public void showPopup(View v) {
+    public void showPopup(View v, final int pos) {
         PopupMenu popupMenu = new PopupMenu(this, v);
         popupMenu.inflate(R.menu.menu_history);
         popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(ViewHistoryActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                if(item.getTitle().equals("Delete")){
+                    db.deletePictureByName(listPicture.get(pos).getName());
+                    listPicture.remove(pos);
+
+
+                    adapter.notifyDataSetChanged();
+                }
+
+
+
+
+
+                return false;
+            }
+        });
+
+//        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                Toast.makeText(ViewHistoryActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+//                AlertDialog.Builder alert = new AlertDialog.Builder(getApplicationContext());
+//                alert.setTitle("Confirm");
+//                alert.setMessage("Do you want to delete this photo?");
+//                alert.setIcon(R.drawable.flashoff);
+//
+//                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        listPicture.remove(pos);
+//                        adapter.notifyDataSetChanged();
+//
+//
+//                    }
+//                });
+//
+//                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//                    }
+//                });
+//
+//                alert.show();
+//                return true;
+//            }
+//        });
 
 
 
